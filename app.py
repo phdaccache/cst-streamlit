fc789e46fd1f7bbd10f176e9e45bc4a1594fdfff
phdaccache <pedrodaccache@gmail.com>
2024-11-13 23:37:51 -0300

    .

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import time
 st.write("hey")
 
 if st.button("generate"):
-    os.system("cd ws3d && ./gradlew build && ls")
+    os.system("cd ws3d && ./gradlew build")
 if st.button("check 1"):
     os.system("ls")
 if st.button("check 2"):
@@ -13,12 +13,12 @@ if st.button("check 2"):
 # os.system("DemoCST/gradlew build")
 # os.system("DemoCST/gradlew run")
 
-# with open("ws3d/build/libs/ws3d-0.0.1-full.jar", "rb") as fp:
-#     btn = st.download_button(
-#         label="Download ws3d JAR",
-#         data=fp,
-#         file_name="ws3d-0.0.1-full.jar",
-#     )
+with open("ws3d/build/libs/ws3d-0.0.1-full.jar", "rb") as fp:
+    btn = st.download_button(
+        label="Download ws3d JAR",
+        data=fp,
+        file_name="ws3d-0.0.1-full.jar",
+    )
 
 
 # with open("DemoCST/build/libs/DemoCST-0.0.4-full.jar", "rb") as fp:
