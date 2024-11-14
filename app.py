2c39197365ad5deeb374b7c65f1239d5b32310e0
phdaccache <pedrodaccache@gmail.com>
2024-11-13 23:13:29 -0300

    .

--- app.py
+++ app.py
@@ -3,23 +3,23 @@ import os
 
 st.write("hey")
 
-# os.system("ws3d/gradlew build")
-# os.system("ws3d/gradlew run")
+os.system("ws3d/gradlew build")
+os.system("ws3d/gradlew run")
 
-os.system("DemoCST/gradlew build")
-os.system("DemoCST/gradlew run")
+# os.system("DemoCST/gradlew build")
+# os.system("DemoCST/gradlew run")
 
-# with open("ws3d/build/libs/ws3d-0.0.1-full.jar", "rb") as fp:
-#     btn = st.download_button(
-#         label="Download ws3d JAR",
-#         data=fp,
-#         file_name="ws3d-0.0.1-full.jar",
-#     )
-
-
-with open("DemoCST/build/libs/DemoCST-0.0.4-full.jar", "rb") as fp:
+with open("ws3d/build/libs/ws3d-0.0.1-full.jar", "rb") as fp:
     btn = st.download_button(
-        label="Download DemoCST JAR",
+        label="Download ws3d JAR",
         data=fp,
-        file_name="DemoCST-0.0.4-full.jar",
+        file_name="ws3d-0.0.1-full.jar",
     )
+
+
+# with open("DemoCST/build/libs/DemoCST-0.0.4-full.jar", "rb") as fp:
+#     btn = st.download_button(
+#         label="Download DemoCST JAR",
+#         data=fp,
+#         file_name="DemoCST-0.0.4-full.jar",
+#     )
