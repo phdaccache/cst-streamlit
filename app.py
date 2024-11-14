91592529f47d2f5264bf10ffec395f754f99a4f7
phdaccache <pedrodaccache@gmail.com>
2024-11-13 23:11:59 -0300

    .

--- app.py
+++ app.py
@@ -2,3 +2,24 @@ import streamlit as st
 import os
 
 st.write("hey")
+
+# os.system("ws3d/gradlew build")
+# os.system("ws3d/gradlew run")
+
+os.system("DemoCST/gradlew build")
+os.system("DemoCST/gradlew run")
+
+# with open("ws3d/build/libs/ws3d-0.0.1-full.jar", "rb") as fp:
+#     btn = st.download_button(
+#         label="Download ws3d JAR",
+#         data=fp,
+#         file_name="ws3d-0.0.1-full.jar",
+#     )
+
+
+with open("DemoCST/build/libs/DemoCST-0.0.4-full.jar", "rb") as fp:
+    btn = st.download_button(
+        label="Download DemoCST JAR",
+        data=fp,
+        file_name="DemoCST-0.0.4-full.jar",
+    )
