5b1d2ae09830b7805d698983106c3fca93a3caae
phdaccache <pedrodaccache@gmail.com>
2024-11-13 23:27:22 -0300

    .

--- app.py
+++ app.py
@@ -4,18 +4,19 @@ import time
 
 st.write("hey")
 
-os.system("ws3d/gradlew build")
-time.sleep(10)
-os.system("cd ws3d && ls")
+if st.button("generate"):
+    os.system("ws3d/gradlew build")
+if st.button("check"):
+    os.system("cd ws3d && ls")
 # os.system("DemoCST/gradlew build")
 # os.system("DemoCST/gradlew run")
 
-with open("ws3d/build/libs/ws3d-0.0.1-full.jar", "rb") as fp:
-    btn = st.download_button(
-        label="Download ws3d JAR",
-        data=fp,
-        file_name="ws3d-0.0.1-full.jar",
-    )
+# with open("ws3d/build/libs/ws3d-0.0.1-full.jar", "rb") as fp:
+#     btn = st.download_button(
+#         label="Download ws3d JAR",
+#         data=fp,
+#         file_name="ws3d-0.0.1-full.jar",
+#     )
 
 
 # with open("DemoCST/build/libs/DemoCST-0.0.4-full.jar", "rb") as fp:
